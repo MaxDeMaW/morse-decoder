07dcff662e739c42f1f3e96ea9ffe1c1972574e8
Maxim Demenkow <demenkow.maxim@gmail.com>
2019-10-05 17:15:11 +0300

    fix: naming comments in English

--- src/index.js
+++ src/index.js
@@ -40,51 +40,41 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-
     var decodeText = String(expr);
-
     //1) get number of letters
     var lettersInExpression = String(expr).length / 10;
     var numberr = "";
+    //receive empty string which will be final decrypt string 
     var str = "";
-
     //get symbol in number
     for (var i = 0; i < lettersInExpression; i++) {
         numberr = decodeText.substring(i * 10, i * 10 + 10);
         if (numberr == "**********") {
-
             str = str + " ";
         }
         else {
-            str = str + DecodeNumber(numberr);
+            str = str + DecryptNumber(numberr);
         }
     }
-
-    // numberr = decodeText.substring(0, 10);
-    // str = str + DecodeNumber(numberr);
-    
-    // numberr = decodeText.substring(10, 20);
-    // str = str + DecodeNumber(numberr);
-
     return str;
 }
 
-function DecodeNumber(numberDecode) {
-    //строку перевести в число без первоначальных нулей
-    var numberWithoutNull = +numberDecode;
-    numberWithoutNull = String(numberWithoutNull);
-    var morzestr = "";
-    //число перевести в морзе
-    //пройтись по числу, разбить на 2 символа
-    //получить морзе значение буквы
-    for (var i = 0; i < 5; i++) {
 
+function DecryptNumber(numberDecode) {
+    //receive number without null (they are in begin)
+    var numberWithoutNull = +numberDecode;
+    //receive string without null (number to string)
+    numberWithoutNull = String(numberWithoutNull);
+    //receive empty string which will be final morze letter 
+    var morzestr = "";
+    //decrypte number to letter
+    //split line to 2 symbols
+    //receive morse value (each 2 symbols = 1 morse sign)
+    for (var i = 0; i < 5; i++) {
         if (numberWithoutNull.substring(i * 2, i * 2 + 2) == "10") morzestr = morzestr + ".";
         if (numberWithoutNull.substring(i * 2, i * 2 + 2) == "11") morzestr = morzestr + "-";
     }
-
-    //найти значение морзе в объекте и получить букву
-
+    //find a morse sign in MORSE_TABLE and get letter
     return MORSE_TABLE[morzestr];
 }
 
